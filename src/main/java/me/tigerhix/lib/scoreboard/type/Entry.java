7c12ce8cfdc1f7cfa8f7ec95f55fe252d4d1649e
TigerHixTang <TigerHixTang@outlook.com>
2026-07-17 23:37:14 +0800

    Polish: use Java 21 repeat(), fix Entry setName(), remove vestigial MANIFEST.MF

--- src/main/java/me/tigerhix/lib/scoreboard/type/Entry.java
+++ src/main/java/me/tigerhix/lib/scoreboard/type/Entry.java
@@ -1,31 +1,32 @@
 package me.tigerhix.lib.scoreboard.type;
-
-import me.tigerhix.lib.scoreboard.common.Strings;
-
-public class Entry {
-
-    private String name;
-    private int position;
-
-    public Entry(String name, int position) {
-        this.name = Strings.format(name);
-        this.position = position;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPosition() {
-        return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
-}
+ 
+ import me.tigerhix.lib.scoreboard.common.Strings;
+ 
+ public class Entry {
+ 
+     private String name;
+     private int position;
+ 
+     public Entry(String name, int position) {
+         this.name = Strings.format(name);
+         this.position = position;
+     }
+ 
+     public String getName() {
+         return name;
+     }
+ 
+     public void setName(String name) {
+         this.name = Strings.format(name);
+     }
+ 
+     public int getPosition() {
+         return position;
+     }
+ 
+     public void setPosition(int position) {
+         this.position = position;
+     }
+ 
+ }
+ 
